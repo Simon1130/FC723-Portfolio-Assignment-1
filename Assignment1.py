0446329d9f27bb6cdee05111dd5b334ed33436ec
Simon1130 <3075302l@student.gla.ac.uk>
2026-03-24 16:24:19 +0000

    Refactoring the code
    quotient seems not use in finding gcd,  and making the structure slightly more clear

--- Assignment1.py
+++ Assignment1.py
@@ -5,31 +5,46 @@ Created on Mon Mar 23 15:36:54 2026
 
 @author: simon
 """
-
-def find_gcd(x,y):
-   
-#Check x or y is bigger
-    if x > y:        
-        bigger = x
-        smaller = y
+'''
+def bigger_or_smaller(input1,input2):
+    if input1 > input2:        
+        bigger = input1
+        smaller = input2
+        
     else:
-        bigger = y
-        smaller = x
-# integer division of bigger and smaller        
-    quotient = bigger // smaller
+        bigger = input2
+        smaller = input1
+        
+        return bigger, smaller
+'''
+
 #modulus of bigger divide by smaller
-    remainder = bigger % smaller
+def find_remainder(x,y):
+    return x % y
+
+def find_gcd(input1,input2):
+#check the input is integer
+    if not isinstance(input1, int) or not isinstance(input2, int):
+        return "This function only accept integer inputs."
+    if input1==0 and input2==0:
+        return "This function can not accept 2 zero inputs."
+    
+#Check x or y is bigger
+    if input1 > input2:        
+        bigger = input1
+        smaller = input2
+    else:
+        bigger = input2
+        smaller = input1
 
 #making a loop to keep find the GCD until remainder hits 0    
-    while remainder != 0:
-    
+    while smaller != 0:
+        remainder = bigger % smaller
         bigger = smaller
         smaller = remainder
         
-        quotient = bigger // smaller
-        remainder = bigger % smaller
 #smaller input and remainder from last term is the GCD        
-    return smaller
+    return bigger
     
-print(find_gcd(1701, 3768))
+print(find_gcd(10, 45))
         
