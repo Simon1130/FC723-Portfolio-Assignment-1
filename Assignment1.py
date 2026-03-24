9b5686c9aa3e76daecade0795036ea309a36401f
Simon1130 <3075302l@student.gla.ac.uk>
2026-03-24 17:43:07 +0000

    Delete some codes that I didnt use
    changing some of the code comment

--- Assignment1.py
+++ Assignment1.py
@@ -5,30 +5,20 @@ Created on Mon Mar 23 15:36:54 2026
 
 @author: simon
 """
-'''
-def bigger_or_smaller(input1,input2):
-    if input1 > input2:        
-        bigger = input1
-        smaller = input2
-        
-    else:
-        bigger = input2
-        smaller = input1
-        
-        return bigger, smaller
-'''
 
 #modulus of bigger divide by smaller
 def find_remainder(x,y):
     return x % y
 
 def find_gcd(input1,input2):
-#check the input is integer
+#check the input is integer, isinstance to check it is integer, using not 
+#if input 1 and 2 not integer
     if not isinstance(input1, int) or not isinstance(input2, int):
         return "This function only accept integer inputs."
-    if input1==0 and input2==0:
+#GCD wont work if two input is 0
+    if input1 == 0 and input2 == 0:
         return "This function can not accept 2 zero inputs."
-    
+
 #Check x or y is bigger
     if input1 > input2:        
         bigger = input1
@@ -43,7 +33,7 @@ def find_gcd(input1,input2):
         bigger = smaller
         smaller = remainder
         
-#smaller input and remainder from last term is the GCD        
+#bigger now contain value of smaller input, smaller and remainder is the GCD
     return bigger
     
 print(find_gcd(10, 45))
